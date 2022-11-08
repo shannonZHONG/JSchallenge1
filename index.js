9061575099c537e5366e73300b8f340aeebc2356
ShannonZhong <zhongshiran@gmail.com>
2022-11-08 13:09:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function subtract() {
 function divide() {
     grabNums()
     const result = num1/num2
-    resultEL.textContent = `Result ${result}`
+    resultEL.textContent = `Result ${Math.round(result)}`
 }
 
 function multiply() {
